6989b49c113ab0c7792f51571dca69c1fe2e706e
tinycarp <132792926+tinycarp@users.noreply.github.com>
2023-05-06 17:27:52 -0700

    uploading files perhaps?
    Little town activate?

---
+++ sketch.js
@@ -0,0 +1,54 @@
+/* This code allows the user to create a little town by pressing the mouse 
+anywhere they choose, which will spawn a random house wherever their mouse is. 
+*/
+
+let homes = [];
+var ground;
+var aspect;
+var desiredImageSize = 600;
+
+function preload () {
+  ground = loadImage ('ground1.png');
+  
+  for (let i = 0; i < 10; i++) {
+    homes[i] = loadImage ('homes/home' + i + '.png')
+  }
+}
+
+
+function setup() {
+  aspect = ground.width/ground.height;
+  
+  createCanvas(desiredImageSize * aspect, desiredImageSize);
+//createCanvas(640, 480); 
+  
+  image(ground, 0, 0, width, height);
+  //now our resized image is stored in the img variable
+  ground = get();
+}
+
+function draw() {
+  background(255);
+  imageMode(CENTER);
+  image(ground, width/2, height/2);
+  
+  if (mousePressed) {
+    createHome (mouseX, mouseY);
+  }
+  
+}
+
+function mousePressed() {
+  createHome(mouseX, mouseY);
+  // prevent default
+  return false;
+}
+
+
+function createHome() {
+  
+  let home = random(homes);
+  image (home, mouseX, mouseY);
+  noLoop();
+}
+
